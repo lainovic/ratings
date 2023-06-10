aeb66b0307af31911758ba0c6033da94f95af4b5
Marko Lainovic <marko.lainovic@gmail.com>
2023-06-10 18:50:28 +0200

    update radar chart code

--- src/RadarChart.jsx
+++ src/RadarChart.jsx
@@ -1,31 +1,41 @@
 import React from 'react';
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 import { Radar } from 'react-chartjs-2';
 
-const RadarChart = () => {
-  // Data for the radar chart
-  const data = {
-    labels: ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label 5'],
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: [5, 7, 3, 9, 6], // Replace with your actual data values
-        backgroundColor: 'rgba(75, 192, 192, 0.4)',
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderWidth: 1,
-      },
-    ],
-  };
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
 
-  // Options for the radar chart
-  const options = {
-    scale: {
-      ticks: { beginAtZero: true, max: 10 },
+export const data = {
+  labels: ['Thing 1', 'Thing 2', 'Thing 3', 'Thing 4', 'Thing 5', 'Thing 6'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [2, 9, 3, 5, 2, 3],
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1,
     },
-  };
+  ],
+};
 
+const RadarChart = () => {
   return (
     <div>
-      <Radar data={data} options={options} />
+      <Radar data={data} />
     </div>
   );
 };
