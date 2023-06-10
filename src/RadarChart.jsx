01bcac580eb94fc3e0a21ec851868282f0a6c49a
Marko Lainovic <marko.lainovic@gmail.com>
2023-06-10 19:26:10 +0200

    play with values

--- src/RadarChart.jsx
+++ src/RadarChart.jsx
@@ -20,11 +20,11 @@ ChartJS.register(
 );
 
 export const data = {
-  labels: ['Thing 1', 'Thing 2', 'Thing 3', 'Thing 4', 'Thing 5', 'Thing 6'],
+  labels: ['Teamwork', 'Innovation', 'Efficiency', 'Results'],
   datasets: [
     {
-      label: '# of Votes',
-      data: [2, 9, 3, 5, 2, 3],
+      label: 'Ratings',
+      data: [2, 7, 3, 5],
       backgroundColor: 'rgba(255, 99, 132, 0.2)',
       borderColor: 'rgba(255, 99, 132, 1)',
       borderWidth: 1,
